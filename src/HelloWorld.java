84f8e677c740bf331bf0b235fa280691bd0a0578
ndayingiye <ndayiringiyedavid120@gmail.com>
2025-06-22 14:58:01 +0200

    feat:/ java arthimatic operation begin

---
+++ src/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        int age = 5;
+        int height = 45;
+        int weight = 67;
+        int accelerator = age + height+ weight -2;
+        System.out.println(accelerator);
+    }
+}
