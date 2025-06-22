3cffef7c9d220b2060caa0528fa37390a665df36
ndayingiye <ndayiringiyedavid120@gmail.com>
2025-06-22 15:45:54 +0200

    feat:/ array looped in java successfully

--- src/javaArraysLoop.java
+++ src/javaArraysLoop.java
@@ -1,9 +1,12 @@
-public class HelloWorld {
+public class javaArraysLoop {
     public static void main(String[] args) {
         int [] ages = new int [5];
         int [] scores = {56,87,89,23,34};
          scores[0] = 100;
         System.out.println(scores[0]);
+        for(int i = 0; i < scores.length; ++i){
+            System.out.println("score" + i +":" + scores[i]);
+        }
 
     }
 }
