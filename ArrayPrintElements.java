f3d3c9be0bd0c451c05c61ef0554d711c2481971
ndayingiye <ndayiringiyedavid120@gmail.com>
2025-06-22 16:24:58 +0200

    feat:/correction task number one

---
+++ ArrayPrintElements.java
@@ -0,0 +1,12 @@
+public class ArrayPrintElements {
+    public static void main(String[] args) {
+       
+int [] heights = new int [5];
+int [] scores = {45,56,78,89,1};
+for(int i = 0 ; i < scores.length; ++i){
+    System.out.println("heights :" + scores[i]);
+}
+
+    }
+}
+
